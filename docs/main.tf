262eda58550c265ba418b3a8f0b11b42e6800f8e
smormen13 <salvamoreno02@gmail.com>
2024-12-13 21:04:19 +0100

    Update main.tf

--- docs/main.tf
+++ docs/main.tf
@@ -8,7 +8,23 @@ terraform {
 }
 
 provider "docker" {
-  host = "npipe:////.//pipe//docker_engine"
+  host = "npipe:////.//pipe//docker_engine" # Para sistemas Windows
+}
+
+# Docker in Docker (DinD)
+resource "docker_image" "dind" {
+  name         = "docker:24.0-dind"
+  keep_locally = false
+}
+
+resource "docker_container" "dind" {
+  image       = docker_image.dind.image_id
+  name        = "practicas-dind"
+  privileged  = true
+  ports {
+    internal = 2375
+    external = 2375
+  }
 }
 
 # Jenkins
@@ -25,41 +41,12 @@ resource "docker_container" "jenkins" {
     external = 8080
   }
 
-  ports {
-    internal = 50000
-    external = 50000
-  }
-
-  volumes = [
-    "C:/jenkins_home:/var/jenkins_home",
-    "//./pipe/docker_engine:/var/run/docker.sock"
+  env = [
+    "JENKINS_ADMIN_ID=admin",
+    "JENKINS_ADMIN_PASSWORD=admin"
   ]
 
   depends_on = [docker_container.dind]
-
-}
-# Docker in Docker (DinD)
-resource "docker_image" "dind" {
-  name         = "docker:24.0-dind"
-  keep_locally = false
-}
-
-resource "docker_container" "dind" {
-  image       = docker_image.dind.image_id
-  name        = "practicas-dind"
-  privileged  = true
-  ports {
-    internal = 2375
-    external = 2375
-  }
-
-  env = [
-    "DOCKER_TLS_CERTDIR="
-  ]
-
-  volumes = [
-    "/var/lib/docker"
-  ]
 }
 
 
